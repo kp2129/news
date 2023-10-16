288784e94ad2afde08d4d348361bc7e89f2e16eb
kp2129 <kristapspolis0@gmail.com>
2023-10-16 11:18:40 +0300

    Izveidoju stila failu un pagaidu html kodu

--- index.php
+++ index.php
@@ -5,14 +5,57 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Ziņas</title>
 </head>
+
 <body>
-    <header></header>
-    <main></main>
+    <header>
+        Logo
+    </header>
+    <div class="zinas-center">
+        <aside>
+            <label>Topic</label>
+            <select>
+                <option>Cars</option>
+            </select>
+            <label>Kārtot</label>
+            <select>
+                <option>Jaunākās</option>
+            </select>
+
+        </aside>
+        <main>
+            <div>
+                <img src="https://www.e90post.com/forums/attachment.php?attachmentid=17216&stc=1&d=1141159310" />
+            </div>
+            <div class="side-news">
+                <div>
+                    <img id="side-news-img" src="https://www.e90post.com/forums/attachment.php?attachmentid=17216&stc=1&d=1141159310" />
+                    <p>Be awere of fake glouries 320d</p>
+                </div>
+                <div>
+                    <div class="side-news-2">
+                        <img src="https://www.e90post.com/forums/attachment.php?attachmentid=17216&stc=1&d=1141159310" />
+                        <p>Cars that look like a BMW 320d e91 </p>
+                    </div>
+
+                </div>
+                <div>
+                    <div class="side-news-2">
+                        <img src="https://www.motortrend.com/uploads/sites/10/2015/11/2006-audi-a4-2.0-t-with-multitronic-sedan-angular-front.png?fit=around%7C875:492.1875" />
+                        <p>Cars that don’t look like a BMW 320d e91 </p>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
+
     <footer></footer>
 </body>
+
 </html>
