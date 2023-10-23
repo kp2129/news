8c4d6d7a5e8e0062bf998c693129093c88920e0c
kp2129 <kristapspolis0@gmail.com>
2023-10-23 10:56:08 +0300

    Mainīju failu struktūru

--- login.php
+++ login.php
@@ -9,6 +9,8 @@ include_once('components/navbar.php')
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="scripts/scripts.js"></script>
     <link rel="stylesheet"  href="styles/style.css">
     <link rel="stylesheet" href="styles/navbar.css">
 
--- post.php
+++ post.php
@@ -10,6 +10,8 @@ include_once('components/navbar.php')
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="scripts/scripts.js"></script>
     <link rel="stylesheet" href="styles/navbar.css">
     <link rel="stylesheet" href="styles/post.css">
     <title>Document</title>
