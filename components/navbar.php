f79b7025c06038d151a351aebe3c04a02cbb251a
amixam <robertobrinko@gmail.com>
2023-10-23 11:34:43 +0300

    admin panels

--- components/navbar.php
+++ components/navbar.php
@@ -1,37 +1,37 @@
-
-    <div class="nav-container">
-        <div class="left-side">
-            <a href="index.php" class="nav-logo">NEWS</a>
-            <div class="topic-container">
-                <button class="topic-button">
-                    Ziņas
-                </button>
-                <button class="topic-button">
-                    Sports
-                </button>
-                <button class="topic-button">
-                    Mūzika
-                </button>
-                <button class="topic-button">
-                    Dārzs
-                </button>
-                <button class="topic-button">
-                    Izklaide
-                </button>
-                <button class="topic-button">
-                    Politika
-                </button>
-            </div>
-        </div>
-        <div class="right-side">
-            <button class="login-button button-style">
-                Pieslēgties
+<!DOCTYPE html>
+<div class="nav-container">
+    <div class="left-side">
+        <a href="index.php" class="nav-logo">NEWS</a>
+        <div class="topic-container">
+            <button class="topic-button">
+                Ziņas
             </button>
-            <button class="filter-button s-button-style">
-                <i class="bi bi-filter"></i>
+            <button class="topic-button">
+                Sports
             </button>
-            <button class="search-button s-button-style">
-                <i class="bi bi-search"></i>
+            <button class="topic-button">
+                Mūzika
+            </button>
+            <button class="topic-button">
+                Dārzs
+            </button>
+            <button class="topic-button">
+                Izklaide
+            </button>
+            <button class="topic-button">
+                Politika
             </button>
         </div>
     </div>
+    <div class="right-side">
+        <button class="login-button button-style">
+            Pieslēgties
+        </button>
+        <button class="filter-button s-button-style">
+            <i class="bi bi-filter"></i>
+        </button>
+        <button class="search-button s-button-style">
+            <i class="bi bi-search"></i>
+        </button>
+    </div>
+</div>
