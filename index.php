8bff557c959728e6c7f85786a28c46b487a8ba53
Sintijaa <ipb21.s.martinsone@vtdt.edu.lv>
2023-11-05 16:38:20 +0200

    uztaisiju index foreach prieks parejiem posts

--- index.php
+++ index.php
@@ -1,5 +1,57 @@
 <?php
+<<<<<<< Updated upstream
 include_once('components/navbar.php')
+=======
+include_once('components/navbar.php');
+include_once('libraries/db.php');
+$db = new Database;
+
+
+
+if (isset($_GET['topic'])) {
+    $topic = $_GET['topic'];
+
+    $allNews = $db->select("SELECT na.*, ai.image_url
+        FROM news_articles na
+        INNER JOIN article_images ai ON na.article_id = ai.article_id
+        INNER JOIN categories c ON na.category_id = c.category_id
+        WHERE c.category_name = '$topic'
+        ORDER BY na.views DESC
+        LIMIT 100 OFFSET 2;
+    ");
+
+    $top2News = $db->select("SELECT na.*, ai.image_url
+        FROM news_articles na
+        INNER JOIN article_images ai ON na.article_id = ai.article_id
+        INNER JOIN categories c ON na.category_id = c.category_id
+        WHERE c.category_name = '$topic'
+        ORDER BY na.views DESC
+        LIMIT 2; 
+    ");
+} else {
+
+    $top2News = $db->select("SELECT na.*, ai.image_url
+        FROM news_articles na
+        INNER JOIN article_images ai ON na.article_id = ai.article_id
+        INNER JOIN categories c ON na.category_id = c.category_id
+        ORDER BY na.views DESC
+        LIMIT 2; 
+    ");
+
+    $allNews = $db->select("SELECT na.*, ai.image_url
+        FROM news_articles na
+        INNER JOIN article_images ai ON na.article_id = ai.article_id
+        INNER JOIN categories c ON na.category_id = c.category_id
+        ORDER BY na.views DESC
+        LIMIT 100 OFFSET 2;
+");
+}
+print_r($allNews);
+
+print_r($top2News);
+
+
+>>>>>>> Stashed changes
 
 ?>
 
@@ -17,6 +69,10 @@ include_once('components/navbar.php')
 </head>
 
 <body>
+<<<<<<< Updated upstream
+=======
+    <?php print_r($_SESSION) ?>
+>>>>>>> Stashed changes
     <!-- saņem no DB visus ierakstus. pirmie 2 ir lielie, pārējie ir mazie varianti -->
     <!-- skatīt pēc figma stila -->
     <div class="container">
@@ -42,34 +98,12 @@ include_once('components/navbar.php')
         </div>
         <!-- mazais variants (pārējiem) -->
         <div class="bottom-container">
-            <a href="#" class="m-post-container">
-                <img class="m-post-image" src="https://ichef.bbci.co.uk/news/976/cpsprodpb/EF66/production/_98268216_gettyimages-826469180-1.jpg.webp" alt="">
-                <p class="m-post-title">LOL JK, Putins izsaka. Mediji pārsteigti par viņu angļu valodas un interneta kultūras izpratni</p>
-            </a>
-
-            <a href="#" class="m-post-container">
-                <img class="m-post-image" src="https://ichef.bbci.co.uk/news/976/cpsprodpb/EF66/production/_98268216_gettyimages-826469180-1.jpg.webp" alt="">
-                <p class="m-post-title">LOL JK, Putins izsaka. Mediji pārsteigti par viņu angļu valodas un interneta kultūras izpratni</p>
-            </a>
-
-            <a href="#" class="m-post-container">
-                <img class="m-post-image" src="https://ichef.bbci.co.uk/news/976/cpsprodpb/EF66/production/_98268216_gettyimages-826469180-1.jpg.webp" alt="">
-                <p class="m-post-title">LOL JK, Putins izsaka. Mediji pārsteigti par viņu angļu valodas un interneta kultūras izpratni</p>
-            </a>
-
-            <a href="#" class="m-post-container">
-                <img class="m-post-image" src="https://ichef.bbci.co.uk/news/976/cpsprodpb/EF66/production/_98268216_gettyimages-826469180-1.jpg.webp" alt="">
-                <p class="m-post-title">LOL JK, Putins izsaka. Mediji pārsteigti par viņu angļu valodas un interneta kultūras izpratni</p>
-            </a>
-            <a href="#" class="m-post-container">
-                <img class="m-post-image" src="https://ichef.bbci.co.uk/news/976/cpsprodpb/EF66/production/_98268216_gettyimages-826469180-1.jpg.webp" alt="">
-                <p class="m-post-title">LOL JK, Putins izsaka. Mediji pārsteigti par viņu angļu valodas un interneta kultūras izpratni</p>
-            </a>
-            <a href="#" class="m-post-container">
-                <img class="m-post-image" src="https://ichef.bbci.co.uk/news/976/cpsprodpb/EF66/production/_98268216_gettyimages-826469180-1.jpg.webp" alt="">
-                <p class="m-post-title">LOL JK, Putins izsaka. Mediji pārsteigti par viņu angļu valodas un interneta kultūras izpratni</p>
-            </a>
-
+            <?php foreach($allNews as $news) : ?>
+                <a href="post.php?id=<?php  echo $news[0]; ?>" class="m-post-container">
+                    <img class="m-post-image" src="<?php echo $news[8]; ?>" alt="">
+                    <p class="m-post-title"><?php  echo $news[1]; ?></p>
+                </a>
+            <?php endforeach; ?>
         </div>
     </div>
 </body>
